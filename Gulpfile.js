df33b830b54b9891f5bda0721faa13841477d998
Anatoliy Ostrovskiy <forgetthisbox@gmail.com>
2015-01-31 21:31:56 +0300

    infra fixes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,28 @@
+'use strict';
+
 var gulp = require('gulp');
 var typescript = require('gulp-tsc');
 var CLIENT_SRC = 'client-src';
 var SERVER_SRC = 'server-src';
 var CLIENT_DIST = 'dist/client';
 var SERVER_DIST = 'dist/server';
+var path = require('path');
+var fs = require('fs');
+var merge = require('merge-stream');
+var walk = function(dir) {
+    var results = []
+    var list = fs.readdirSync(dir)
+    list.forEach(function(file) {
+        file = dir + '/' + file
+        var stat = fs.statSync(file)
+        if (stat && stat.isDirectory()) results = results.concat(walk(file))
+        else results.push(file)
+    })
+    return results
+};
 
-gulp.task('client-js', function() {
+
+gulp.task('client-js', ['components'], function() {
     return gulp.src([
             CLIENT_SRC + '/ts/the-game.ts'
         ])
@@ -26,16 +43,48 @@ gulp.task('images', function() {
         .pipe(gulp.dest(CLIENT_DIST + '/images/'));
 });
 
-gulp.task('components', ['client-js'], function() {
-    gulp.src('bower_components/**')
+gulp.task('bower_components', function() {
+    return gulp.src('bower_components/**')
         .pipe(gulp.dest(CLIENT_DIST + '/bower_components'));
-    gulp.src(CLIENT_SRC + '/components/**')
+});
+
+gulp.task('components-copy', ['bower_components'], function() {
+    return gulp.src(CLIENT_SRC + '/components/**')
         .pipe(gulp.dest(CLIENT_DIST + '/components'));
 });
 
+gulp.task('components', ['components-copy'], function(callback) {
+    var filesToCompile = [];
+
+    walk(path.resolve(CLIENT_DIST + '/components/')).forEach(function(file) {
+        if (/\/ts\/index\.ts$/.test(file)) {
+            filesToCompile.push(file);
+        }
+    });
+
+    return merge.apply(null, filesToCompile.map(function(file) {
+        console.log(file, 'hello');
+        return gulp.src(file)
+            .pipe(typescript({
+                target: 'ES5',
+                out: 'index.js'
+            }))
+            .pipe(gulp.dest(file.replace(/\/ts\/index\.ts$/, '')));
+    }));
+});
+
+gulp.task('clean-ts-files', ['components'], function(callback) {
+    walk(CLIENT_DIST + '/components').forEach(function(file) {
+        if (/\/ts\/index\.ts$/.test(file)) {
+            console.log('Removing', file);
+            fs.removeFileSync(file);
+        }
+    });
+    callback();
+});
 
 gulp.task('copy-server', function() {
-    gulp.src(SERVER_SRC + '/**')
+    return gulp.src(SERVER_SRC + '/**')
         .pipe(gulp.dest(SERVER_DIST ));
 });
 
@@ -51,6 +100,6 @@ gulp.task('server-js', ['copy-server'], function() {
         .pipe(gulp.dest(SERVER_DIST + '/'));
 });
 
-gulp.task('client', ['html', 'client-js', 'components', 'images']);
+gulp.task('client', ['html', 'components', 'clean-ts-files', 'images']);
 gulp.task('server', ['copy-server', 'server-js']);
 gulp.task('default', ['client', 'server']);
---
+++ d.ts/polymer.d.ts
@@ -0,0 +1,5 @@
+declare var Polymer: (name:string, attributes?:any) => void;
+
+interface Window {
+    Polymer: any;
+}
